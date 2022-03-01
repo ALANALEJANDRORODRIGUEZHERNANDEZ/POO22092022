029becf7670d0ad102935de730d197b5d6c73c4b
ALANALEJANDRORODRIGUEZHERNANDEZ <alanrdzzhdzz@gmail.com>
2022-03-01 12:52:51 -0600

    1 de Marzo, Automovil

--- clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -4,6 +4,9 @@
  */
 package conceptosbasicos;
 
+import ICO.fes.Silla;
+import ICO.fes.Automovil;
+import java.awt.Color;
 /**
  *
  * @author alan
@@ -14,7 +17,21 @@ public class ConceptosBasicos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola de nuevo"); // TODO code application logic here
+        System.out.println("Hola de nuevo");
+        Silla s1 = new Silla();
+        System.out.println(s1); //por defecto se invoca toString();
+        System.out.println(s1.toString());
+        
+        Automovil miBocho = new Automovil();
+        miBocho.setMarca("VW");
+        miBocho.setSubMarca("Sedan");
+        miBocho.setModelo(1970);
+        miBocho.setColor(Color.blue);
+        System.out.println(miBocho);
+        Automovil miMustang = new Automovil("Ford","Mustan",2010,Color.yellow);
+        System.out.println(miMustang);
+        Automovil miAKura = new Automovil("Akura","NSX",2013,Color.cyan);
+        System.out.println(miAKura);
     }
     
 }
