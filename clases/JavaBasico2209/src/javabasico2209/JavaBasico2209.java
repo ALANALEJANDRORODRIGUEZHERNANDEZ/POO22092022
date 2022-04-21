4b9eb9e87cf78462e7f4ee7b159db8a06d0b54f2
ALANALEJANDRORODRIGUEZHERNANDEZ <alanrdzzhdzz@gmail.com>
2022-04-21 14:17:32 -0500

    clase 21 04 22

--- clases/JavaBasico2209/src/javabasico2209/JavaBasico2209.java
+++ clases/JavaBasico2209/src/javabasico2209/JavaBasico2209.java
@@ -48,7 +48,87 @@ public class JavaBasico2209 {
         JOptionPane.showMessageDialog(null, "Hola mundo","Aqui va el titulo", JOptionPane.ERROR_MESSAGE);
         String dato = JOptionPane.showInputDialog(null,"Dame tu edad","Introducir datos",JOptionPane.INFORMATION_MESSAGE);
         System.out.println(dato);
+        treel.crecer(2.5f);
         
+        int edad_3 = 15;
+        String resultado = "";
+        if (edad_3 < 18) {
+            resultado = "Menor de edad";
+        }
+        else{
+            resultado = "Mayor de edad";
+        }
+        
+        System.out.println(resultado);
+        
+        //Con código terniario
+        int edad_4 = 24;
+        String res = "";
+        res = edad_4 < 18? "Eres menor de edad" : "Eres mayor de edad";
+        System.out.println(res);
+        
+        //Menor cantidad de lineas de código posible
+        int edad_5 = 15;
+        System.out.println(edad_5 < 18? "Escuicle" : "NO escuincle");
+        
+        System.out.println("-------------------------------------------------------");
+        
+        int val_1 = 12;
+        int val_2 = 27;
+        
+        System.out.println(val_1 | val_2);
+        
+        int val_3 = 1;
+        int val_4 = 0;
+        val_4 = val_3 << 2;
+        System.out.println(val_4);
+        
+        System.out.println("-------------------------Arreglos-------------------------------");
+        int[] edades;
+        edades = new int[5];
+        System.out.println(edades);
+        
+        edades[0] = 10;
+        System.out.println("La primer edad es: " + edades[0]);
+        
+        int estaturas[] = new int[5];
+        System.out.println(estaturas);
+        
+        int pesos[] = {86,99,56,76,77};
+        System.out.println(pesos[0]);
+        System.out.println(pesos[1]);
+        System.out.println(pesos[2]);
+        System.out.println(pesos[3]);
+        System.out.println(pesos[4]);
+        
+        //Imprimir lo anterior pero bien
+        System.out.println("for");
+        for (int i = 0; i < pesos.length; i++) {
+            System.out.println(pesos[i]);
+        }
+        System.out.println("--------------------Inverso--------------------");
+        for (int i = pesos.length-1; i >= 0; i--) {
+            System.out.println(pesos[i]);
+        }
+        
+        System.out.println("--------------------------------------------------------------");
+        Alumno lista[] = new Alumno[5];
+        lista[0] = new Alumno("45565212", 2, 9.0f);
+        lista[1] = new Alumno("56123185", 2, 7.0f);
+        lista[2] = new Alumno("74842315", 2, 6.7f);
+        lista[3] = new Alumno("78231544", 2, 8.2f);
+        lista[4] = new Alumno("32110584", 2, 10.0f);
+        
+        for (int i = 0; i < lista.length; i++) {
+            Alumno alumno = lista[i];
+            alumno.checkPerformance();
+        }
+        
+        System.out.println("foreach");
+        
+        for (Alumno alumno : lista) {
+            alumno.checkPerformance();
+        }
         
     }
     
