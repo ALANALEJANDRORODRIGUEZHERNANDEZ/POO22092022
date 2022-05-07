e7434cc2a8a6693656e12e6cb149c54897d679ff
ALANALEJANDRORODRIGUEZHERNANDEZ <alanrdzzhdzz@gmail.com>
2022-05-07 14:57:08 -0500

    Codigo 05/05/2022

---
+++ tareas/JavaBasico2209Parte2/src/javabasico2209parte2/JavaGraficoMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javabasico2209parte2;
+import ico.fes.awt.Ventana;
+import ico.fes.awt.VentanaV2;
+/**
+ *
+ * @author alan
+ */
+public class JavaGraficoMain {
+    public static void main(String[] args) {
+        VentanaV2 window = new VentanaV2();
+    }
+}
